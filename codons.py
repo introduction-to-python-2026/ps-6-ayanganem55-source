fa781bf9287452e8a1b619d59369f7ded22bb363
ayanganem55-source <ayanganem55@gmail.com>
2025-12-07 18:41:42 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,8 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    def create_codon_dict(file_path):           codon_dict = {}     
+with open(file_path, 'r') as file:                 for line in file:             
+parts = line.strip().split('\t')             
+if len(parts) >= 3:                
+codon = parts[0]                 amino_acid = parts[2]                 codon_dict[codon] = amino_acid     return codon_dict
 
 
